702e2ab17e77cd81e27256bb01fc167a8770c6f0
blau <le.space@icloud.com>
2023-07-23 01:57:51 +0900

    Setup Test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,21 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+# gem minitest-reporters
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  # 並列テスト
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    # seedデータをテストデータとして読み込み
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
+  # workers: プロセス数を示す（2以上の場合並列テストが有効になる）
+  # number_of_processors： 使用しているマシン(Docker)のコア数
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
